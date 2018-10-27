c436d561061e9abe5fa34ef8a87fa1f548b5c1e5
Srinivas <srinivasdamam@gmail.com>
2018-10-27 13:01:52 +0530

    Floating Points :)

---
+++ FloatingPoints/src/com/srinivas/Main.java
@@ -0,0 +1,21 @@
+package com.srinivas;
+
+/**
+ * Gallon to liters conversion
+ * 1 Gallon = 3.7854 liters
+ * Task: Output x liters to Gallons
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        double gallons;
+        double gallonToLiter = 3.7854;
+
+        // Output 5 liters in gallons
+        gallons = 5;
+
+        double fiveGallonsToLiters = gallons * gallonToLiter;
+
+        System.out.println("5 gallons in liters " + fiveGallonsToLiters);
+    }
+}
