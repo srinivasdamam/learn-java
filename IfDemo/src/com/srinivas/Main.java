ef84befe3664f7212951185298462ffa40a9e4a9
Srinivas <srinivasdamam@gmail.com>
2018-10-27 14:31:46 +0530

    If condition

---
+++ IfDemo/src/com/srinivas/Main.java
@@ -0,0 +1,15 @@
+package com.srinivas;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int counter;
+	    for(counter = 1; counter <= 30; counter++) {
+            System.out.println(counter +  " gallon in liters: " + counter * 3.7854);
+            if (counter % 10 == 0) {
+                System.out.println();
+            }
+        }
+
+    }
+}
