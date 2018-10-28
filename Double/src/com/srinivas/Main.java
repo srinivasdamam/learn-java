4ef2433853a6c4074fdc7218886b22563240b6dd
Srinivas <srinivasdamam@gmail.com>
2018-10-28 15:13:49 +0530

    Double

---
+++ Double/src/com/srinivas/Main.java
@@ -0,0 +1,22 @@
+package com.srinivas;
+
+/**
+ * Find the length of hypotenuse using
+ * Pythogorous theorem
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        int var1 = 5;
+        char ch = 'a';
+        var1 = 'f';
+        System.out.println(var1);
+        double a, b;
+        double c;
+        a = 5; b = 5;
+        c = Math.sqrt(a*a + b*b);
+
+        System.out.println(c);
+    }
+}
